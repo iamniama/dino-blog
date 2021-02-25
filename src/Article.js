69a35a703e7dc48aff09bd3b009252a546f86feb
iamniama <plectrumonthespectrum@gmail.com>
2021-02-24 17:23:12 -0700

    tweaks to modal, changed input to textarea

--- src/Article.js
+++ src/Article.js
@@ -7,6 +7,7 @@ import React, { useState } from 'react';
 
 function Article(props) {
     const [show, setShow] = useState(false);
+    const [show2, setShow2] = useState(false)
     const [postData, setPostData] = useState(props.body)
     const [tempData, setTempData] = useState(postData)
     const handleClose = () => {
@@ -31,8 +32,8 @@ function Article(props) {
             <div className="ar-body">
                 <p>{post.body}</p>
             </div>
-            <Button variant="primary" size="sm" onClick={handleShow}>
-                Launch Bootstrap Modal
+            <Button variant="outline-dark" size="sm" onClick={handleShow}>
+                Edit Post
             </Button>
             <Modal show={show} onHide={handleClose}>
 
